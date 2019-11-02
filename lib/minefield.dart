6c4e0188742c5f8f5cb2d3d0aeef8a3d3dfab5c7
Nicolas Picon <nicopico.dev@gmail.com>
2019-11-02 22:29:05 +0100

    Draw uncovered cells

--- lib/minefield.dart
+++ lib/minefield.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:minesweeper/cell.dart';
 
 const double CELL_SIZE = 30;
+const double BEZEL_SIZE = 2;
 
 class Minefield extends StatelessWidget {
   final int width;
@@ -28,7 +29,7 @@ class Minefield extends StatelessWidget {
       rows.add(TableRow(children: cells));
     }
     return Table(
-      border: TableBorder.all(),
+      border: TableBorder.all(color: Color(0xFFADADAD)),
       defaultColumnWidth: FixedColumnWidth(CELL_SIZE),
       children: rows,
     );
@@ -42,15 +43,62 @@ class MineCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: CELL_SIZE,
-      width: CELL_SIZE,
-      child: Center(
-        child: Text(
-          cellData.bomb ? "X" : "",
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+    return CustomPaint(
+      size: Size.square(CELL_SIZE),
+      painter: _CellPainter(cellData),
     );
+  }
+}
+
+class _CellPainter extends CustomPainter {
+  CellData cellData;
+
+  final Color bgPaint = const Color(0xFFBDBDBD);
+
+  _CellPainter(this.cellData);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    drawUncovered(canvas, size);
+  }
+
+  @override
+  bool shouldRepaint(_CellPainter oldDelegate) {
+    return oldDelegate.cellData != cellData;
+  }
+
+  void drawUncovered(Canvas canvas, Size size) {
+    // Background
+    var paint = Paint()
+      ..color = const Color(0xFFBDBBBE)
+      ..style = PaintingStyle.fill;
+
+    canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), paint);
+
+    // Top-left
+    paint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = BEZEL_SIZE;
+
+    var path = Path();
+    path.moveTo(size.width, 0);
+    path.relativeLineTo(-BEZEL_SIZE, BEZEL_SIZE);
+    path.relativeLineTo(-size.width + 2 * BEZEL_SIZE, 0);
+    path.relativeLineTo(0, size.height - 2 * BEZEL_SIZE);
+    canvas.drawPath(path, paint);
+
+    // Bottom-right
+    paint = Paint()
+      ..color = Color(0xFF7D797C)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = BEZEL_SIZE;
+
+    path.reset();
+    path.moveTo(size.width, 0);
+    path.relativeLineTo(-BEZEL_SIZE, BEZEL_SIZE);
+    path.relativeLineTo(0, size.height - 2 * BEZEL_SIZE);
+    path.relativeLineTo(-size.width + 2 * BEZEL_SIZE, 0);
+    canvas.drawPath(path, paint);
   }
 }
