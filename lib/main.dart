80e164f417d0da9d0f1f10928756b75f7e63f566
Nicolas Picon <nicopico.dev@gmail.com>
2020-02-16 19:39:44 +0100

    Add AppCenter integrations for crash and analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minesweeper/crash_report.dart';
 import 'package:minesweeper/screen/game/game_screen.dart';
 
 void main() => runApp(MyApp());
@@ -7,8 +8,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    CrashReportHelper.initialize();
     return MaterialApp(
-      title: 'Minesweeper Demo',
+      title: 'Minesweeper',
       home: GameScreen(),
     );
   }
