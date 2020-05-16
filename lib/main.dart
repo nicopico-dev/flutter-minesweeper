af64100fac8059f5ef4853a6568ac548ad5329c9
Nicolas Picon <nicolas.picon@in-tact.fr>
2020-05-16 19:06:16 +0200

    Keep difficulty preferences across launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:minesweeper/domain/difficulty_state.dart';
 import 'package:minesweeper/screen/game/game_screen.dart';
+import 'package:minesweeper/service/preferences.dart';
 import 'package:provider/provider.dart';
 
 import 'domain/game_state.dart';
@@ -11,15 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<DifficultyState>(
-      create: (_) => DifficultyState(),
+      create: (_) => DifficultyState(preferences: Preferences()),
       child: ChangeNotifierProxyProvider<DifficultyState, GameState>(
         create: (context) {
-          var difficulty =
-              Provider.of<DifficultyState>(context, listen: false).difficulty;
-          return GameState(difficulty: difficulty);
+          var state = Provider.of<DifficultyState>(context, listen: false);
+          return GameState(difficulty: state.difficulty);
         },
-        update: (_, difficultyState, gameState) =>
-            gameState..difficulty = difficultyState.difficulty,
+        update: (_, difficultyState, gameState) {
+          return gameState..difficulty = difficultyState.difficulty;
+        },
         child: MaterialApp(
           title: 'Minesweeper Demo',
           home: GameScreen(),
